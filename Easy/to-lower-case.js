1d84c553f0dd5c3f9cafd84fa096b9494a954ee7
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-05-26 19:32:53 -0300

    to-lower-case
    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.

---
+++ Easy/to-lower-case.js
@@ -0,0 +1,11 @@
+/* Given a string s, return the string after replacing every uppercase letter with the same lowercase letter. */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+ console.log(s.toLowerCase())
+ return s.toLowerCase()
+};
+
+toLowerCase("Hello")
