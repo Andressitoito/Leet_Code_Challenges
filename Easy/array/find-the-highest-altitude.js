b0aadc76e337723b979dec08fba19f2c15eba0ed
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-06-19 15:54:01 -0300

    find-the-highest-altitude
    There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
    
    You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.

---
+++ Easy/array/find-the-highest-altitude.js
@@ -0,0 +1,26 @@
+/* There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point. */
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function (gain) {
+	const highest_arr = [0];
+
+	for (let i = 0; i < gain.length; i++) {
+		const last_hight = highest_arr[highest_arr.length - 1];
+
+		const actual_hight = last_hight + gain[i];
+
+		highest_arr.push(actual_hight);
+	}
+
+	const max = Math.max(...highest_arr);
+
+	console.log(max);
+
+	return max;
+};
+
+largestAltitude([-5, 1, 5, 0, -7]);
