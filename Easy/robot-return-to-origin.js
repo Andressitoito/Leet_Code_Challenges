6b418c0010a6bf6d45abe5ac019dbdcf990f5bbc
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-07-29 12:30:24 -0300

    robot-return-to-origin
     There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
    
    You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
    
    Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.
    
    Note: The way that the robot is facing is irrelevant. 'R' will always make the robot move to the right once, 'L' will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.

---
+++ Easy/robot-return-to-origin.js
@@ -0,0 +1,43 @@
+/* There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+
+You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
+
+Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.
+
+Note: The way that the robot is "facing" is irrelevant. 'R' will always make the robot move to the right once, 'L' will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move. */
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function (moves) {
+ let final = [0, 0];
+
+ for (let i = 0; i < moves.length; i++) {
+  switch (moves[i]) {
+   case "U":
+    final[0]++;
+    break;
+
+   case "D":
+    final[0]--;
+    break;
+
+   case "L":
+    final[1]++;
+    break;
+
+   case "R":
+    final[1]--;
+    break;
+  }
+ }
+
+ const result = final[0] === 0 && final[1] === 0
+
+ console.log(result)
+
+ return result
+};
+
+judgeCircle("LDRRLRUULR");
+judgeCircle("RU");
