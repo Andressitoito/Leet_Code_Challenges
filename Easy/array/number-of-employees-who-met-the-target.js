bc3c6b9a5133c1ee605739804f2c0b43837d5241
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-08-03 18:56:52 -0300

    number-of-employees-who-met-the-target
    There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
    
    The company requires each employee to work for at least target hours.
    
    You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
    
    Return the integer denoting the number of employees who worked at least target hours.

---
+++ Easy/array/number-of-employees-who-met-the-target.js
@@ -0,0 +1,25 @@
+/* There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
+
+The company requires each employee to work for at least target hours.
+
+You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+
+Return the integer denoting the number of employees who worked at least target hours. */
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function (hours, target) {
+	let employee = 0;
+
+	for (let i = 0; i < hours.length; i++) {
+		hours[i] >= target && employee++;
+	}
+
+	console.log(employee);
+
+	return employee;
+};
+
+numberOfEmployeesWhoMetTarget([0, 1, 2, 3, 4], 2);
