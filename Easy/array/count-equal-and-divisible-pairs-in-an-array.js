3d52e3bb189cdb05ac134859f00d759bf44f6ac7
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-04-11 10:17:15 -0300

    count-equal-and-divisible-pairs-in-an-array
    Updated the code for better runtime and memory.

--- Easy/array/count-equal-and-divisible-pairs-in-an-array.js
+++ Easy/array/count-equal-and-divisible-pairs-in-an-array.js
@@ -8,12 +8,8 @@ var countPairs = function (nums, k) {
 	let count = 0;
 
 	for (let i = 0; i < nums.length; i++) {
-		const numI = nums[i];
-
 		for (let j = i + 1; j < nums.length; j++) {
-			const numJ = nums[j];
-
-			if (numI === numJ && (i * j) % k === 0) {
+			if (nums[i] === nums[j] && (i * j) % k === 0) {
 				count++;
 			}
 		}
