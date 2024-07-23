fd45b273c532d97a3438f30124c4933bd2bc7bb0
Andresito <60946504+Andressitoito@users.noreply.github.com>
2024-07-23 19:09:23 -0300

    number-of-changing-keys
    `You are given a 0-indexed string s typed by a user. Changing a key is defined as using a key different from the last used key. For example, s = "ab" has a change of a key while s = "bBBb" does not have any.
    
    Return the number of times the user had to change the key.
    
    Note: Modifiers like shift or caps lock won't be counted in changing the key that is if a user typed the letter 'a' and then the letter 'A' then it will not be considered as a changing of key.`

---
+++ Easy/array/number-of-changing-keys.js
@@ -0,0 +1,30 @@
+/* `You are given a 0-indexed string s typed by a user. Changing a key is defined as using a key different from the last used key. For example, s = "ab" has a change of a key while s = "bBBb" does not have any.
+
+Return the number of times the user had to change the key.
+
+Note: Modifiers like shift or caps lock won't be counted in changing the key that is if a user typed the letter 'a' and then the letter 'A' then it will not be considered as a changing of key.` */
+/**
+	* @param {string} s
+	* @return {number}
+	*/
+var countKeyChanges = function (s) {
+	s = s.toLowerCase();
+	let response = 0;
+	last = s[0];
+
+	for (let i = 1; i < s.length; i++) {
+		const char = s[i];
+
+		if (char === last) {
+		} else {
+			last = char;
+			response++;
+		}
+	}
+
+	console.log("response ", response)
+
+	return response;
+};
+
+countKeyChanges("aAbBcC");
