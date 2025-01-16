1c4b3029911746ba8c2b40bbe1ff11fb69890da7
Andresito <60946504+Andressitoito@users.noreply.github.com>
2025-01-16 17:37:10 -0300

    WIP count-substrings-that-satisfy-k-constraint-i
    First algorithm approach

---
+++ Easy/string/count-substrings-that-satisfy-k-constraint-i.js
@@ -0,0 +1,57 @@
+/* You are given a binary string s and an integer k.
+
+A binary string satisfies the k-constraint if either of the following conditions holds:
+
+The number of 0's in the string is at most k.
+The number of 1's in the string is at most k.
+Return an integer denoting the number of 
+substrings of s that satisfy the k-constraint. */
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var countKConstraintSubstrings = function (s, k) {
+	console.log("s k ", s, k);
+
+	const substrings = [];
+	let response = 0;
+
+	let count = 0;
+
+	for (let i = s.length - 1; i >= 0; i--) {
+		const index = s[i];
+
+		for (let j = i + 1; j >= 0; j--) {
+			count++;
+			console.log("count ", count);
+
+			const arr = s.substring(j, i);
+
+			console.log("subarr ", arr);
+
+			const ones = arr.split("").filter((char) => char === "1").length;
+			const zeroes = arr.split("").filter((char) => char === "0").length;
+
+			console.log("zeroes, oneos ", ones, zeroes);
+
+			if (ones > k || zeroes > k) {
+				console.log("not ");
+				continue;
+			} else if (ones === 0 && zeroes === 0) {
+				continue;
+			} else {
+				response++;
+			}
+		}
+
+	}
+  
+	console.log("response ", response);
+  
+  return response;
+};
+
+countKConstraintSubstrings("10101", 1);
+
+
